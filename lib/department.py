668ed3ecc20cb0d46afb4aa1085866a0eeceda0f
bryankiriama <bryankiriama@gmail.com>
2025-11-26 09:51:06 +0300

    created orm

--- lib/department.py
+++ lib/department.py
@@ -10,3 +10,15 @@ class Department:
 
     def __repr__(self):
         return f"<Department {self.id}: {self.name}, {self.location}>"
+
+@classmethod
+def create_table(cls):
+    sql = """
+    CREATE TABLE IF NOT EXISTS departsments(
+    id INTEGER PRIMARY,
+    name TEXT,
+     location TEXT
+     )
+    """
+    CURSOR.executes(sql)
+    CONN.commit()
